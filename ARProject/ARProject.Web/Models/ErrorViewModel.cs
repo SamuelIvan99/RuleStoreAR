df15bc8b03d94fb524ed1750335b0fb27a8db22b
Samuel Ivan <1081504@ucn.dk>
2021-09-01 10:06:56 +0200

    Solution project update.

---
+++ ARProject/ARProject.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ARProject.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
