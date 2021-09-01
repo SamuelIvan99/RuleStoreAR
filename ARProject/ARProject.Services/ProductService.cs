4b629e33fc23a40f57adac2df09cdbf1ecd82998
Samuel Ivan <1081504@ucn.dk>
2021-09-01 14:11:51 +0200

    New products added to the list.

--- ARProject/ARProject.Services/ProductService.cs
+++ ARProject/ARProject.Services/ProductService.cs
@@ -12,9 +12,25 @@ namespace ARProject.Services
             {
                 Id = 1,
                 Source = "https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.glb?1542147958948",
-                IOSSource = "https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.usdz?v=1569545377878,",
+                IOSSource = "https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.usdz?v=1569545377878",
                 Image = "https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717",
                 QRSource = "https://api.qrserver.com/v1/create-qr-code/?data=https://192.168.0.98:44352/Product/1&amp;size=100x100"
+            },
+            new Product()
+            {
+                Id = 2,
+                Source = "",
+                IOSSource = "",
+                Image = "https://images.freeimages.com/images/large-previews/fde/aliens-1-1373417.jpg",
+                QRSource = "https://api.qrserver.com/v1/create-qr-code/?data=https://192.168.0.98:44352/Product/2&amp;size=100x100"
+            },
+            new Product()
+            {
+                Id = 3,
+                Source = "",
+                IOSSource = "",
+                Image = "https://images.sadhguru.org/sites/default/files/media_files/iso/en/64083-natures-temples.jpg",
+                QRSource = "https://api.qrserver.com/v1/create-qr-code/?data=https://192.168.0.98:44352/Product/3&amp;size=100x100"
             },
         };
 
